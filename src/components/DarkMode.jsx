88cb8ea4dce9aa36920666065fa4b53b5dc460a3
devflaco <npressure00@gmail.com>
2024-05-13 18:13:14 -0300

    added favicon + title

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -35,39 +35,3 @@ const DarkMode = () => {
 
 export default DarkMode;
 
-
-
-// import React from 'react'
-// import { MdSunny } from "react-icons/md";
-// import { IoIosMoon } from "react-icons/io";
-
-// const DarkMode = () => {
-//     const [theme, setTheme] = React.useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
-
-//     const element = document.documentElement;
-//     console.log(Element);
-
-//     React.useEffect(() => {
-//         localStorage.setItem("theme", theme);
-//         if (theme == "dark") {
-//             element.classList.add("dark")
-//             element.classList.add("dark");
-//         } else {
-//             element.classList.remove("light")
-//             element.classList.remove("dark");
-//         }
-//     })
-
-//     return (
-//         <div className=' relative'>
-//             <MdSunny
-//                 onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
-//                 src={<MdSunny />} alt="" className={`w-18 cursor-pointer absolute right-0 z-10 ${theme == "dark" ? "opacity-0" : "opacity-100"} transition-all duration-300`} />
-//             <IoIosMoon
-//                 onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
-//                 src={<IoIosMoon />} alt="" className={`w-18 cursor-pointer`} />
-//         </div>
-//     )
-// }
-
-// export default DarkMode
