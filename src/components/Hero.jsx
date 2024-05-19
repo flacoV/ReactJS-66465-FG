1daed5975089c4f088af2f4dd6a8a6f10952cda2
devFlaco <npressure00@gmail.com>
2024-05-19 19:02:33 -0300

    added Hero & images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import Slider from 'react-slick'
 
 const Hero = () => {
+
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: 4000,
+        cssEase: "easi-in-out",
+        pauseOnHover: false,
+        pauseOnFocus: true,
+      };
+
   return (
     <div>
-        <div>
+        <div className='w-full '>
             {/* Hero Section */}
+
+        <Slider { ...settings}>
+        <div className='w-full'>
+        <h3>1</h3>
+      </div>
+      <div className='w-full'>
+        <h3>2</h3>
+      </div>
+      <div className='w-full'>
+        <h3>3</h3>
+      </div>
+      <div className='w-full'>
+        <h3>4</h3>
+      </div>
+      <div className='w-full'>
+        <h3>5</h3>
+      </div>
+      <div className='w-full'>
+        <h3>6</h3>
+      </div>
+        </Slider>
+
         </div>
     </div>
   )
