c1766dc5f8b2eb4a0033f9c7aa42b8088b130eef
devFlaco <npressure00@gmail.com>
2024-05-19 19:50:50 -0300

    added hero content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,44 @@
 import React from 'react'
 import Slider from 'react-slick'
+import Image1 from '../assets/hero/greenHs.png'
+import Image2 from '../assets/hero/macbookPro.png'
+import Image3 from '../assets/hero/ps5.png'
+import Image4 from '../assets/hero/rtx4090.png'
+
+const HeroData = [
+    {
+        id: 1,
+        img: Image1,
+        subtitle: 'Razer Gaming',
+        title: 'Wireless',
+        title2: 'Headphone',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+    },
+    {
+        id: 2,
+        img: Image2,
+        subtitle: 'Apple',
+        title: 'Macbook Pro',
+        title2: 'M2 Chip',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+    },
+    {
+        id: 3,
+        img: Image3,
+        subtitle: 'Sony',
+        title: 'Play Station 5',
+        title2: '1TB Version',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+    },
+    {
+        id: 4,
+        img: Image4,
+        subtitle: 'Nvidia',
+        title: '4090',
+        title2: 'RTX Series',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+    },
+]
 
 const Hero = () => {
 
@@ -17,31 +56,41 @@ const Hero = () => {
       };
 
   return (
-    <div>
-        <div className='w-full '>
+    <div className='container'>
+        <div className='overflow-hidden rounded-3xl min-h-[550px] 
+        sm:min-h-[650px] hero-bg-color flex justify-center items-center'>
             {/* Hero Section */}
-
+        <div className='container pb-8 sm:pb-0'>
         <Slider { ...settings}>
-        <div className='w-full'>
-        <h3>1</h3>
-      </div>
-      <div className='w-full'>
-        <h3>2</h3>
-      </div>
-      <div className='w-full'>
-        <h3>3</h3>
-      </div>
-      <div className='w-full'>
-        <h3>4</h3>
-      </div>
-      <div className='w-full'>
-        <h3>5</h3>
-      </div>
-      <div className='w-full'>
-        <h3>6</h3>
-      </div>
+            {
+             HeroData.map((data) => (
+                <div key={data.id}>
+                    <div className='grid grid-cols-1 sm:grid-cols-2'>
+                     {/* Text content section */}
+                        <div className='flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 
+                        text-center sm:text-left order-2 sm:order-1
+                        relative z-10 '>
+                            <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold'>{data.subtitle}</h1>
+                            <h2 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h2>
+                            <h2 className='text-5xl uppercase text-white dark:text-white/5 
+                            sm:text-[80px] md;text-[100px] xl:text-[150px] font-bold'>{data.title2}</h2>
+                            <div>
+                                <button>Shop Now</button>
+                            </div>
+                        </div>
+                     {/* Img section */}
+                     <div className='order-1 sm:order-2'>
+                        <div>
+                            <img src={data.img} alt="" className='w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 
+                            object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40' />
+                        </div>
+                     </div>
+                    </div>
+                </div>
+             ))
+            }
         </Slider>
-
+        </div>
         </div>
     </div>
   )
