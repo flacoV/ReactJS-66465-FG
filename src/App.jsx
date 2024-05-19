4545885fa7b709bab1b99321d491aa6f71fbc43c
devFlaco <npressure00@gmail.com>
2024-05-19 18:40:27 -0300

    added menu link

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
-
 import React from 'react'
 import Navbar from './components/Navbar'
-
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 import { IoMdSearch } from "react-icons/io"
 import { FaCartShopping } from "react-icons/fa6"
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { HiTrendingUp } from "react-icons/hi";
+import { BsFire } from "react-icons/bs";
+import { IoStar } from "react-icons/io5";
 import DarkMode from './DarkMode'
+
+
+const dropDownLinks = [
+    {
+        id: 1,
+        icon: <HiTrendingUp />,
+        name: 'Trending Products',
+        link: '/#'
+    },
+    {
+        id: 2,
+        icon: <BsFire />,
+        name: 'Best Sellig',
+        link: '/#'
+    },
+    {
+        id: 3,
+        icon: <IoStar />,
+        name: 'Top Rated',
+        link: '/#'
+    },
+]
 
 const Navbar = () => {
     return (
@@ -18,12 +44,35 @@ const Navbar = () => {
                                 <li><a href="" className='inline-block px-4 font-semibold text-gray-400 hover:text-black dark:hover:text-white duration-200'>Shop</a></li>
                                 <li><a href="" className='inline-block px-4 font-semibold text-gray-400 hover:text-black dark:hover:text-white duration-200'>About</a></li>
                                 <li><a href="" className='inline-block px-4 font-semibold text-gray-400 hover:text-black dark:hover:text-white duration-200'>Blog</a></li>
+                                <li className='relative cursor-pointer group'>
+                                    <a href="#" className='flex items-center gap-[2px] font-semibold text-gray-400 dark:hover:text-white py-2'>
+                                        Quick Links
+                                        <span>
+                                            <IoIosArrowDropdownCircle className='group-hover:rotate-180 duration-300' />
+                                        </span>                                    
+                                    </a>
+                                    <div className='absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-2 dark:text-white'>
+                                        <ul className='space-y-2'>
+                                            {
+                                                dropDownLinks.map((data, index) => (
+                                                    <li className=' flex items-center text-gray-400 hover:text-black dark:hover:text-white dark:hover:bg-brWhite/20 duration-200 hover:bg-primary/20 rounded-md p-1'>
+                                                            {data.icon}
+                                                        <a className='text-gray-400 hover:text-black rounded-md dark:hover:text-white duration-200 inline-block w-full p-2 font-semibold'
+                                                        href={data.link}>
+                                                             {data.name}
+                                                        </a>
+                                                    </li>
+                                                ))
+                                            }
+                                        </ul>
+                                    </div>
+                                </li>
                             </ul>
                         </div>
                     </div>
                     <div className='flex justify-between items-center gap-4'>
                         <div className='relative group hidden sm:block'>
-                            <input type="text" placeholder='Search' className='search-bar' />
+                            <input type="text" placeholder='Search a product' className='search-bar' />
                             <IoMdSearch className='text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200' />
                         </div>
                         <button className='relative p-3'>
