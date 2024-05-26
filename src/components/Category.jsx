7f5185f0d39fd5588fac884ba6b924732902690b
devFlaco <npressure00@gmail.com>
2024-05-26 18:11:55 -0300

    fixed darkmode bug

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,7 +6,7 @@ import Image3C from '../assets/category/macbook.png'
 
 const Category = () => {
   return (
-    <div className='py-8'>
+    <div className='py-8 dark:bg-gray-900'>
         <div className='container'>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
             {/* Primera Columna */}
