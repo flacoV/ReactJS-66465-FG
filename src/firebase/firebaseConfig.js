fd8b0fc89bfdec421418581e01be3df72743692a
devFlaco <npressure00@gmail.com>
2024-06-28 19:11:20 -0300

    change db config

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,27 +4,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: 'AIzaSyD6RT-J6OuZ1AHLWJ-f2E8pVuQI1NMgx_4',
+  authDomain: 'tech-store-ch.firebaseapp.com',
+  projectId: 'tech-store-ch',
+  storageBucket: 'tech-store-ch.appspot.com',
+  messagingSenderId: '591117465811',
+  appId: '1:591117465811:web:391f15f25a939e24647c26'
 };
 
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-const fetchProducts = async () => {
-  try {
-    const querySnapshot = await getDocs(collection(db, "products"));
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-    });
-  } catch (error) {
-    console.error("Error fetching products: ", error);
-  }
-};
-
-fetchProducts();
